905364bca7f0989cd46b8d410dab76fb11f31bc5
santoshtiwari41 <santoshhh066@gmail.com>
2022-10-24 13:34:11 +0545

    initial commit

---
+++ counterApp/app.js
@@ -0,0 +1,16 @@
+count=0;
+document.getElementById("increase").addEventListener('click',function()
+{
+    count++;
+   document.getElementById("countt").innerHTML=count;
+})
+document.getElementById("decrease").addEventListener('click',function()
+{
+    count--;
+   document.getElementById("countt").innerHTML=count;
+})
+
+if(count>0){
+    document.getElementById("countt").style.color=green;
+
+}
